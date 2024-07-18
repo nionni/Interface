410e1493068646380046fc77d1149478d400f7d6
nioni <70058439+nionni@users.noreply.github.com>
2024-07-17 22:04:18 -0300

    Create FileLoader.java

---
+++ FileLoader.java
@@ -0,0 +1,13 @@
+package javacore.Minterfaces.Dominio;
+
+public class FileLoader implements DataLoader, DataRemover{
+    @Override
+    public void load() {
+        System.out.println("Carregando dados de um arquivo...");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados de um arquivo...");
+    }
+}

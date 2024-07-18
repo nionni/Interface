96d4fc97e4bbbe6bc71dfd3b3f91d57e884819cd
nioni <70058439+nionni@users.noreply.github.com>
2024-07-17 22:03:54 -0300

    Create DataBaseLoader.java

---
+++ DataBaseLoader.java
@@ -0,0 +1,13 @@
+ackage javacore.Minterfaces.Dominio;
+
+public class DataBaseLoader implements DataLoader, DataRemover {
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco de dados...");
+    }
+
+    @Override
+    public void remove() {
+        System.out.println("Removendo dados do banco de dados...");
+    }
+}
